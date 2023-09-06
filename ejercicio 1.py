33506aced5ee15fd686ae588399a772e71827b0f
Julian <bienvenidomartinez634@gmail.com>
2023-09-06 00:30:15 -0300

    subo ejer1 y ejer2

---
+++ ejercicio 1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     05/09/2023
+# Copyright:   (c) Usuario 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    def arearectangulo ():
+        base = 7
+        altura = 12
+        print (base*altura)
+    arearectangulo()
+    main()
+
---
+++ ejercicio 2.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     05/09/2023
+# Copyright:   (c) Usuario 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    def areacirculo ():
+        pi = 3.14159
+        radio = 5
+        print ((pi*radio)**2)
+    areacirculo()
+    main()
